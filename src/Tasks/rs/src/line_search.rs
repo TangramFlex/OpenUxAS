043dc38b7a69c7d549713a9b74f2e04be7842c8a
Adam C. Foltzer <acfoltzer@galois.com>
2018-08-13 11:44:46 -0700

    add the "plan both directions" option to the line search MDM
    This is instead of using the `TaskOption` XML tag when configuring the `TaskManagerService`

--- src/Tasks/rs/src/line_search.rs
+++ src/Tasks/rs/src/line_search.rs
@@ -12,10 +12,6 @@ pub async fn run_task(task_msg: LineSearchTask, mut task: LineSearch) {
 
 impl LineSearch {
     fn configure(&mut self, task_msg: &LineSearchTask) {
-        // aspirational assignment; currently this option is set in
-        // the XML configuration of the `TaskServiceManager`, but it
-        // really should be part of the messages that spin up tasks
-
-        // self.plan_both_directions = task_msg.plan_both_directions
+        self.plan_both_directions = task_msg.either_direction;
     }
 }
