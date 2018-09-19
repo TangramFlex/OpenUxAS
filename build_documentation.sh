14d1b96d2fc3d6976e6337ab1e31b4e8e786ba65
David B. Lamkins <dlamkins@galois.com>
2018-09-19 11:28:24 -0700

    Add dnf support so we can build on Fedora.

--- build_documentation.sh
+++ build_documentation.sh
@@ -21,6 +21,7 @@ if [ "$(uname)" == "Darwin" ]; then
     # Install firefox, sed, evince (for pdf viewing)
     brew install firefox sed evince
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    if [ -n "$(which apt)" ]; then
     # Install doxygen and related packages: in terminal
     sudo apt -y install doxygen
     sudo apt -y install graphviz
@@ -28,6 +29,11 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     sudo apt -y install texlive-latex-extra
     # Install sed, evince (for pdf viewing)
     sudo apt -y install sed evince
+    fi
+    if [ -n "$(which dnf)" ]; then
+    sudo dnf -y install doxygen graphviz texlive texlive-latex texlive-tufte-latex texlive-tabu texlive-hardwrap texlive-multirow texlive-titlesec texlive-adjustbox texlive-import texlive-sectsty texlive-tocloft
+    sudo dnf -y install zathura zathura-pdf-poppler
+    fi
 else
     echo "Unsupported platform! Script install only for Linux and Mac"
     exit 1
