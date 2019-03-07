55c8c77326f0b2d0d2f3100702a6129125d754e6
David B. Lamkins <dlamkins@galois.com>
2019-03-07 14:48:10 -0800

    Consolidate stop.

--- examples/01_HelloWorld/runUxAS_HelloWorld-dual.sh
+++ examples/01_HelloWorld/runUxAS_HelloWorld-dual.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 echo "NOTICE: Running HelloWorld dual example from task01 executable."
-echo "Use \`./stop\` in a separate window (on this directory) to stop."
+echo "When running, press Return to stop."
 echo "Press enter to continue"
 read
 BIN="../../build/task01"
 $BIN -cfgPath cfg_HelloWorld-1.xml &
 $BIN -cfgPath cfg_HelloWorld-2.xml &
-
+read && pkill -9 task01
