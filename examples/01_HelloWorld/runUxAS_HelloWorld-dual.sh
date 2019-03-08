353f54ccb2c84cb8072ff0d10326da9204b4555d
David B. Lamkins <dlamkins@galois.com>
2019-03-08 14:04:36 -0800

    Ensure proper termination.

--- examples/01_HelloWorld/runUxAS_HelloWorld-dual.sh
+++ examples/01_HelloWorld/runUxAS_HelloWorld-dual.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
 echo "NOTICE: Running HelloWorld dual example from task01 executable."
-echo "When running, press Return to stop."
-echo "Press enter to continue"
+echo "Press enter to start the demo."
+echo "When running, press Enter to stop."
 read
 BIN="../../build/task01"
 $BIN -cfgPath cfg_HelloWorld-1.xml &
 $BIN -cfgPath cfg_HelloWorld-2.xml &
+trap 'echo **** PRESS ENTER TO TERMINATE ****' INT
 read && pkill -9 task01
