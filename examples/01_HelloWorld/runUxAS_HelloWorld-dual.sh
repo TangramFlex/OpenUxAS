e7ab5a33b817287c8bf5776ba404b9c28baff6d9
David B. Lamkins <dlamkins@galois.com>
2019-03-12 12:11:21 -0700

    Update demo scripts.

--- examples/01_HelloWorld/runUxAS_HelloWorld-dual.sh
+++ examples/01_HelloWorld/runUxAS_HelloWorld-dual.sh
@@ -5,7 +5,7 @@ echo "Press enter to start the demo."
 echo "When running, press Enter to stop."
 read
 BIN="../../build/task01"
-$BIN -cfgPath cfg_HelloWorld-1.xml &
-$BIN -cfgPath cfg_HelloWorld-2.xml &
+$BIN -cfgPath cfg_HelloWorld-1.xml |tee /dev/tty >hello-1.log &
+$BIN -cfgPath cfg_HelloWorld-2.xml |tee /dev/tty >hello-2.log &
 trap 'echo **** PRESS ENTER TO TERMINATE ****' INT
 read && pkill -9 task01
--- examples/01_HelloWorld/runUxAS_HelloWorld.sh
+++ examples/01_HelloWorld/runUxAS_HelloWorld.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
 echo "NOTICE: Running HelloWorld example from task01 executable."
-echo "Press enter to continue"
+echo "Press enter to start the demo."
+echo "When running, press Enter to stop."
 read
 BIN="../../build/task01"
-$BIN -cfgPath cfg_HelloWorld.xml
-
+$BIN -cfgPath cfg_HelloWorld.xml |tee /dev/tty >hello.log &
+trap 'echo **** PRESS ENTER TO TERMINATE ****' INT
+read && pkill -9 task01
