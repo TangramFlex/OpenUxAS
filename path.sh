209b0a0c1ad7a3a66483e3d081fc17e9439eef64
David B. Lamkins <dlamkins@galois.com>
2019-03-14 15:39:31 -0700

    Idempotence.

--- path.sh
+++ path.sh
@@ -1,5 +1,5 @@
 # Source this script using a POSIX-compatible shell (bash, dash, sh, ...)
 # to set PATH for UxAS development.
 export PYTHONUSERBASE=`pwd`/toolroot
-export PATH=$PYTHONUSERBASE/bin:$PATH
-export PATH=$PATH:`pwd`/experimental
+[[ "$PATH" = *$PYTHONUSERBASE/bin:* ]] || export PATH=$PYTHONUSERBASE/bin:$PATH
+[[ "$PATH" = *:`pwd`/experimental* ]] || export PATH=$PATH:`pwd`/experimental
