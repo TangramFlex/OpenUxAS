1557a67f2cdb54d0a8f1c0cd4b568b19ed3be22a
David B. Lamkins <dlamkins@galois.com>
2019-03-15 14:12:47 -0700

    Recognize Ubuntu's UFW firewall.

--- examples/01_HelloWorld/runUxAS_HelloWorld-dual.sh
+++ examples/01_HelloWorld/runUxAS_HelloWorld-dual.sh
@@ -8,6 +8,28 @@ nokill () {
 	echo
 	echo
 }
+firewall_off () {
+	if which ufw >/dev/null 2>&1; then
+		sudo ufw disable
+	elif which systemctl 2>&1 >/dev/null; then
+		sudo systemctl stop firewalld
+	else
+		echo "Unknown firewall"
+		exit 1
+	fi
+	echo "Firewall stopped"
+}
+firewall_on () {
+	if which ufw >/dev/null 2>&1; then
+		sudo ufw enable
+	elif which systemctl 2>&1 >/dev/null; then
+		sudo systemctl restart firewalld
+	else
+		echo "Unknown firewall"
+		exit 1
+	fi
+	echo "Firewall started"
+}
 echo "NOTICE: Running HelloWorld dual example from task01 executable."
 echo "You'll be prompted by sudo for your password to temporarily"
 echo "disable the firewall."
@@ -15,11 +37,10 @@ echo
 echo "Press enter to start the demo."
 echo "When running, press Enter to stop."
 read
-sudo systemctl stop firewalld
-echo firewall stopped
+firewall_off
 sleep 1
 BIN="../../build/task01"
 $BIN -cfgPath cfg_HelloWorld-1.xml 2>&1 |tee /dev/tty >hello-1.log &
 $BIN -cfgPath cfg_HelloWorld-2.xml 2>&1 |tee /dev/tty >hello-2.log &
 trap nokill INT
-read && { pkill -9 task01; sudo systemctl restart firewalld; }
+read && { pkill -9 task01; firewall_on; }
